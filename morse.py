1286c32e96ae11ce85ac5b53833bcceb83c089f8
Sundae Chen <sundaechn@gmail.com>
2017-10-19 19:33:26 +0800

    Update morse.py

--- morse.py
+++ morse.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# @Date    : 2017-10-19 19:32:08
+# @Author  : Sundae Chen (sundaechn@gmail.com)
+# @Link    : http://sundae.applinzi.com/home
+
 
 CODE = {'A': '.-', 'B': '-...',   'C': '-.-.',
         'D': '-..',    'E': '.',      'F': '..-.',
@@ -15,56 +20,59 @@ CODE = {'A': '.-', 'B': '-...',   'C': '-.-.',
         '6': '-....',  '7': '--...',  '8': '---..',
         '9': '----.',  ' ': ' '}
 CODE1 = {'A': '.-', 'B': '-...',   'C': '-.-.',
-        'D': '-..',    'E': '.',      'F': '..-.',
-        'G': '--.',    'H': '....',   'I': '..',
-        'J': '.---',   'K': '-.-',    'L': '.-..',
-        'M': '--',     'N': '-.',     'O': '---',
-        'P': '.--.',   'Q': '--.-',   'R': '.-.',
-        'S': '...',    'T': '-',      'U': '..-',
-        'V': '...-',   'W': '.--',    'X': '-..-',
-        'Y': '-.--',   'Z': '--..',
+         'D': '-..',    'E': '.',      'F': '..-.',
+         'G': '--.',    'H': '....',   'I': '..',
+         'J': '.---',   'K': '-.-',    'L': '.-..',
+         'M': '--',     'N': '-.',     'O': '---',
+         'P': '.--.',   'Q': '--.-',   'R': '.-.',
+         'S': '...',    'T': '-',      'U': '..-',
+         'V': '...-',   'W': '.--',    'X': '-..-',
+         'Y': '-.--',   'Z': '--..',
 
-        '0': '-----',  '1': '.----',  '2': '..---',
-        '3': '...--',  '4': '....-',  '5': '.....',
-        '6': '-....',  '7': '--...',  '8': '---..',
-        '9': '----.'}
-UNCODE = dict(map(lambda t:(t[1],t[0]),CODE.items()))
-UNCODE1 = dict(map(lambda t:(t[1],t[0]),CODE1.items()))
+         '0': '-----',  '1': '.----',  '2': '..---',
+         '3': '...--',  '4': '....-',  '5': '.....',
+         '6': '-....',  '7': '--...',  '8': '---..',
+         '9': '----.'}
+UNCODE = dict(map(lambda t: (t[1], t[0]), CODE.items()))
+UNCODE1 = dict(map(lambda t: (t[1], t[0]), CODE1.items()))
 
 
 def morse(msg):
-    word=''
-    morse=msg
+    word = ''
+    morse = msg
     for char in morse:
-        word=word+CODE[char.upper()] + ' '
+        word = word+CODE[char.upper()] + ' '
     return word.encode('utf-8')
+
 
 def umorse(msg):
-    word=''
-    msg=msg.split(' ')
+    word = ''
+    msg = msg.split(' ')
     for char in msg:
-        if char=='':
-            word=word+' '
+        if char == '':
+            word = word+' '
         else:
-            word=word+UNCODE[char]
+            word = word+UNCODE[char]
     return word.encode('utf-8')
+
 
 def morse1(msg):
-    word=''
-    morse=msg
+    word = ''
+    morse = msg
     for char in morse:
-        if char==' ':
-            word=word
+        if char == ' ':
+            word = word
         else:
-            word=word+CODE1[char.upper()]+'/'
+            word = word+CODE1[char.upper()]+'/'
     return word.encode('utf-8')
 
+
 def umorse1(msg):
-    word=''
-    msg=msg.split('/')
+    word = ''
+    msg = msg.split('/')
     for char in msg:
-        if char=='':
-            word=word+' '
+        if char == '':
+            word = word+' '
         else:
-            word=word+UNCODE1[char]
+            word = word+UNCODE1[char]
     return word.encode('utf-8')
