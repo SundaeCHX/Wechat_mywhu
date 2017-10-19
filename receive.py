2e0205afdaa80b4bee917d752f30839861f7b52c
Sundae Chen <sundaechn@gmail.com>
2017-10-19 19:35:45 +0800

    Update receive.py

--- receive.py
+++ receive.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# filename: receive.py
+# @Date    : 2017-10-19 19:32:08
+# @Author  : Sundae Chen (sundaechn@gmail.com)
+# @Link    : http://sundae.applinzi.com/home
+
+
 import xml.etree.ElementTree as ET
+
 
 def parse_xml(web_data):
     if len(web_data) == 0:
@@ -16,31 +22,41 @@ def parse_xml(web_data):
     elif msg_type == 'image':
         return ImageMsg(xmlData)
 
+
 class Msg(object):
+
     def __init__(self, xmlData):
         self.ToUserName = xmlData.find('ToUserName').text
         self.FromUserName = xmlData.find('FromUserName').text
         self.CreateTime = xmlData.find('CreateTime').text
         self.MsgType = xmlData.find('MsgType').text
         self.MsgId = xmlData.find('MsgId').text
-        
+
+
 class EventMsg(Msg):
+
     def __init__(self, xmlData):
         Msg.__init__(self, xmlData)
         self.Event = xmlData.find('Event').text.encode("utf-8")
-        
+
+
 class TextMsg(Msg):
+
     def __init__(self, xmlData):
         Msg.__init__(self, xmlData)
         self.Content = xmlData.find('Content').text.encode("utf-8")
 
+
 class ImageMsg(Msg):
+
     def __init__(self, xmlData):
         Msg.__init__(self, xmlData)
         self.PicUrl = xmlData.find('PicUrl').text
         self.MediaId = xmlData.find('MediaId').text
 
+
 class FormatMsg(Msg):
+
     def __init__(self, xmlData):
         Msg.__init__(self, xmlData)
         self.media_id = xmlData.find('MediaId').text
